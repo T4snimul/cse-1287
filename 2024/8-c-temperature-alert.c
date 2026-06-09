8e7437db226fc8ab17e5025b0ad869db6084b27f
Tasnimul Hasan <2408020@student.ruet.ac.bd>
2026-06-09 18:55:59 +0600

    fix: use function as the the question requirement

--- 2024/8-c-temperature-alert.c
+++ 2024/8-c-temperature-alert.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
+void printTemperatures(float temperatures[], int index, int size)
+{
+  // Base case
+  if (index >= size)
+    return;
+
+  printf("%.2f", temperatures[index]);
+
+  if (temperatures[index] > 100)
+    printf(" ALERT!");
+
+  printf("\n");
+
+  // Recursive case
+  printTemperatures(temperatures, index + 1, size);
+}
+
 int main()
 {
   float temperatures[5] = {98.5f, 76.23f, 101.83f, 88.12f, 105.55f};
 
   printf("Sensor readings\n");
-  for (int i = 0; i < 5; i++)
-  {
-    printf("%0.2f", *(temperatures + i));
 
-    if (*(temperatures + i) > 100)
-      printf(" ALERT!");
-
-    printf("\n");
-  }
+  printTemperatures(temperatures, 0, 5);
 
   return 0;
 }
